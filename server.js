f352f3f2687c97af1e26b72979e06288947606d9
Kapten boneng <iwak01bogo@gmail.com>
2026-04-02 09:06:39 +0700

    Update server.js

--- server.js
+++ server.js
@@ -10,3 +10,15 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log("Server jalan di port " + PORT);
 });
+app.get("/pi/account", async (req, res) => {
+  try {
+    const result = await rpc.getAccount?.() || {
+      status: "not_implemented",
+      message: "rpc.getAccount belum tersedia"
+    };
+
+    res.json(result);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
